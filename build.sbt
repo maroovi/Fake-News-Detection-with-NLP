cea4f3de2596b7e8bada0c920f49317e2d667304
nvnkumar45 <nvnkumar45@gmail.com>
2021-04-08 18:55:15 -0400

    Initial Commit with data load from CSV

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "Fake-News-Detection-with-NLP"
+
+version := "0.1"
+
+scalaVersion := "2.12.12"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.2.3" % "test",
+  "org.apache.spark" %% "spark-core" % "3.0.1",
+  "org.apache.spark" %% "spark-sql" % "3.0.1",
+  "org.apache.spark" %% "spark-mllib" % "2.4.0"
+)
---
+++ src/main/scala/Data.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.lit
+object Data extends App{
+  val session = SparkSession.builder()
+    .master("local")
+    .appName("Create DF using CSV")
+    .getOrCreate()
+
+  val real = session.read
+    .options(Map("header"->"true","quote"->"\"","escape"->"\""))
+    .csv("src/test/scala/resources/datasets/true.csv")
+
+  val fake = session.read
+    .options(Map("header"->"true","quote"->"\"","escape"->"\""))
+    .csv("src/test/scala/resources/datasets/fake.csv")
+
+  real.withColumn("target",lit("real"))
+  fake.withColumn("target",lit("fake"))
+
+  val total_data = real.union(fake)
+
+  total_data.select("subject").distinct().show()
+  println(total_data.count())
+
+}
