dcf412b6f8b7a53285bee177b686a4a2ae9064f8
nvnkumar45 <nvnkumar45@gmail.com>
2021-04-17 03:04:43 -0400

    Added several functions and Main method

--- src/main/scala/Data.scala
+++ src/main/scala/Data.scala
@@ -1,12 +1,21 @@
-import org.apache.spark.sql.functions.lit
-import org.apache.spark.sql.{DataFrame, SparkSession}
-object Data extends App{
-  def create_dataframe():DataFrame = {
-    val session = SparkSession.builder()
-      .master("local")
-      .appName("Create DF using CSV")
-      .getOrCreate()
+import org.apache.spark.ml.feature._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.{col, lit, udf}
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+object Data{
+  val session: SparkSession = SparkSession.builder()
+    .master("local")
+    .appName("Create DF using CSV")
+    .getOrCreate()
 
+  /*
+   * Creating Dataframe from the csv dataset
+   */
+  def create_dataframe():DataFrame = {
+
+    // Load and parse the data file, converting it to a DataFrame.
     val real = session.read
       .options(Map("header" -> "true", "quote" -> "\"", "escape" -> "\""))
       .csv("src/test/scala/resources/datasets/true.csv")
@@ -14,9 +23,6 @@ object Data extends App{
     val fake = session.read
       .options(Map("header" -> "true", "quote" -> "\"", "escape" -> "\""))
       .csv("src/test/scala/resources/datasets/fake.csv")
-
-    //real.withColumn("target", lit(0))
-    //fake.withColumn("target", lit(1))
 
     val total_data = real.withColumn("target", lit(0)).union(fake.withColumn("target", lit(1)))
 
@@ -26,5 +32,44 @@ object Data extends App{
 
     total_data
   }
-  create_dataframe().show(10)
+
+  /*
+   * Cleaning the data - removing punctuations from the column position specified
+   */
+  def clean_data(dataFrame: DataFrame, position: Int, newColumnName : String):DataFrame={
+    val test_with_no_punct: Seq[String] = dataFrame.collect().map(_.getString(position).replaceAll("https?://\\S+\\s?", "").
+      replaceAll("""[\p{Punct}]""", "")).toSeq
+
+    val rdd: RDD[String] = session.sparkContext.parallelize(test_with_no_punct)
+    val rdd_train: RDD[Row] = dataFrame.rdd.zip(rdd).map(r => Row.fromSeq(r._1.toSeq ++ Seq(r._2)))
+    val df = session.createDataFrame(rdd_train, dataFrame.schema.add(newColumnName, StringType))
+
+    df
+  }
+
+  /*
+   * Preprocessing of a specified column
+   */
+  def preprocess_data(dataFrame: DataFrame,columnName:String):DataFrame={
+
+    val countTokens_train:UserDefinedFunction = udf { (words: Seq[String]) => words.length }
+
+    // Extract tokens
+    val tokenizer = new RegexTokenizer()
+      .setInputCol(columnName)
+      .setOutputCol(columnName+"_words")
+      .setPattern("\\W")
+    val token_df: DataFrame = tokenizer.transform(dataFrame)
+    token_df.select(columnName,columnName+"_words").withColumn("tokens",countTokens_train(col(columnName+"_words"))).show()
+
+    // Remove stop words
+
+    val sw_remover = new StopWordsRemover()
+      .setInputCol(columnName+"_words")
+      .setOutputCol(columnName+"_sw_removed")
+    sw_remover.transform(token_df)
+    val sw_remover_df: DataFrame = sw_remover.transform(token_df).withColumn("tokens", countTokens_train(col(columnName+"_sw_removed")))
+
+    sw_remover_df
+  }
 }
--- src/main/scala/TFIDF.scala
+++ src/main/scala/TFIDF.scala
@@ -1,175 +1,82 @@
-import org.apache.spark.ml.feature.{CountVectorizer, CountVectorizerModel, HashingTF, IDF, RegexTokenizer, StopWordsRemover, StringIndexer, Tokenizer, VectorAssembler}
-import org.apache.spark.mllib.linalg
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.expressions.UserDefinedFunction
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
-import Data._
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.{NaiveBayes, RandomForestClassificationModel, RandomForestClassifier}
+import org.apache.spark.ml.classification.{NaiveBayes, RandomForestClassifier}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.internal.SQLConf
-import org.apache.spark.sql.types.StringType
-import org.spark_project.dmg.pmml.True
-object TFIDF extends App {
-
-  val sparkSession = SparkSession.builder // To create a spark session
-    .master("local")
-    .appName("TFIDF")
-    .getOrCreate()
-
-  sparkSession.sparkContext.setLogLevel("ERROR")
-
-  val df = create_dataframe()
-
-  val test_with_no_punct: Seq[String] = df.collect().map(_.getString(3).replaceAll("https?://\\S+\\s?", "").
-    replaceAll("""[\p{Punct}]""", "")).toSeq
-
-  val countTokens_train:UserDefinedFunction = udf { (words: Seq[String]) => words.length }
-
-  val title_tokenizer = new RegexTokenizer() // Extract tokens from title
-    .setInputCol("title")
-    .setOutputCol("title_words")
-    .setPattern("\\W")
-
-  val title_token_df: DataFrame = title_tokenizer.transform(df)
-
-  title_token_df.select("title","title_words").withColumn("tokens",countTokens_train(col("title_words"))).show(false)
-
-  val title_sw_remover = new StopWordsRemover() // Remove stop words from title
-    .setInputCol("title_words")
-    .setOutputCol("title_sw_removed")
-
-  title_sw_remover.transform(title_token_df)
-
-  val title_sw_remover_df: DataFrame = title_sw_remover.transform(title_token_df).withColumn("tokens", countTokens_train(col("title_sw_removed")))
-
-  val title_count_vectorizer: CountVectorizerModel = new CountVectorizer() // Compute Term frequency from title
-    .setInputCol("title_sw_removed")
-    .setOutputCol("tf_title")
-    .setVocabSize(3)
-    .setMinDF(2)
-    .fit(title_sw_remover_df)
-
-  val title_count_vectorizer_df: DataFrame= title_count_vectorizer.transform(title_sw_remover_df)
-
-  val idf = new IDF().setInputCol("tf_title").setOutputCol("title_tfidf")
-
-  val idfModel = idf.fit(title_count_vectorizer_df)
-
-  val rescaledData = idfModel.transform(title_count_vectorizer_df)
-
-  rescaledData.show(10)
-
-/// text
-
-
-  val text_tokenizer = new RegexTokenizer() // Extract tokens from text
-    .setInputCol("text")
-    .setOutputCol("text_words")
-    .setPattern("\\W")
-
-  val text_token_df: DataFrame = text_tokenizer.transform(rescaledData)
-
-  text_token_df.select("text","text_words").withColumn("tokens",countTokens_train(col("text_words"))).show(false)
-
-  val text_sw_remover = new StopWordsRemover() // Remove stop words from title
-    .setInputCol("text_words")
-    .setOutputCol("text_sw_removed")
-
-  text_sw_remover.transform(text_token_df)
-
-  val text_sw_remover_df: DataFrame = text_sw_remover.transform(text_token_df).withColumn("tokens", countTokens_train(col("text_sw_removed")))
-
-  val text_count_vectorizer: CountVectorizerModel = new CountVectorizer() // Compute Term frequency from title
-    .setInputCol("text_sw_removed")
-    .setOutputCol("tf_text")
-    .setVocabSize(3)
-    .setMinDF(2)
-    .fit(text_sw_remover_df)
-
-  val text_count_vectorizer_df: DataFrame= text_count_vectorizer.transform(text_sw_remover_df)
-
-  val idf_text = new IDF().setInputCol("tf_text").setOutputCol("text_tfidf")
-
-  val idftextModel = idf_text.fit(text_count_vectorizer_df)
-
-  val rescaledtextData = idftextModel.transform(text_count_vectorizer_df)
-
-  println("Text")
-  rescaledtextData.show(10)
-
-
-  ////
-
-  val indexer = new StringIndexer().setInputCol("subject").setOutputCol("subject_idx")
-
-  //val subject_str_indexed = indexer.fit(rescaledtextData).transform(rescaledtextData)  #########
-
-  val assembler = new VectorAssembler().setInputCols(Array("title_tfidf","text_tfidf","subject_idx")).setOutputCol("features")
-
-  //val vec_assembler = assembler.transform(subject_str_indexed)  #######
-
-  //vec_assembler.show(10)
-
-
-  //// Splitting Data Frame
-
-  val Array(train_data, test_data) = rescaledtextData.randomSplit(Array(0.7,0.3))
-
-
-  ///// Random Forest Classifier
-
-  val rf = new RandomForestClassifier().setLabelCol("target").setFeaturesCol("features").setNumTrees(20).setPredictionCol("fake_predict").setMaxDepth(7)
-
-  val pipeline = new Pipeline().setStages(Array(indexer,assembler,rf))
-
-  val model = pipeline.fit(train_data)
-
-  val predictions = model.transform(test_data)
-
-  predictions.show(10)
-
-  val evaluater = new MulticlassClassificationEvaluator().setLabelCol("target").setPredictionCol("fake_predict").setMetricName("accuracy")
-
-  val accuracy = evaluater.evaluate(predictions)
-
-  println("Random Forest Accuracy = "+accuracy)
-
-  ////////////////////////////////////////////
-  //val rfModel = model.stages(2).asInstanceOf[RandomForestClassificationModel].write.overwrite.save("src/test/scala/resources/model/myRandomForestClassificationModel")
-
-  //sparkSession.sparkContext.parallelize(Seq(model),1).saveAsObjectFile("src/test/scala/resources/model/RFC")
-  //println(s"Learned classification forest model:\n ${rfModel.toDebugString}")
-
-  //model.save("src/test/scala/resources/model/myRandomForestClassificationModels") --- TO SAVE THE MODEL
-
-
-  ///////// Naive Bayes
-
-  val nv = new NaiveBayes().setLabelCol("target").setFeaturesCol("features")
-
-  val nv_pipeline = new Pipeline().setStages(Array(indexer,assembler,nv))
-
-
-  val nv_model = nv_pipeline.fit(train_data)
-
-  val nv_prediction = nv_model.transform(test_data)
-
-  println("Navie Bayes Model")
-  println("")
-  nv_prediction.show(10)
-
-
-  val nv_evaluater = new MulticlassClassificationEvaluator().setLabelCol("target").setPredictionCol("prediction").setMetricName("accuracy")
-
-
-  val nv_accuracy = nv_evaluater.evaluate(nv_prediction)
-
-  println(nv_accuracy)
-
-  nv_model.write.overwrite().save("src/test/scala/resources/model/NaiveBayes")
-
+import org.apache.spark.ml.feature._
+import org.apache.spark.sql.DataFrame
+object TFIDF{
+
+  def tf_idf(dataFrame: DataFrame,columnName: String):DataFrame={
+
+    // Compute Term frequency vectors
+    val count_vectorizer: CountVectorizerModel = new CountVectorizer()
+      .setInputCol(columnName+"_sw_removed")
+      .setOutputCol(columnName+"_ct_vectorized")
+      .setVocabSize(3)
+      .setMinDF(2)
+      .fit(dataFrame)
+    val count_vectorizer_df: DataFrame= count_vectorizer.transform(dataFrame)
+
+    // Performing IDF for extracting features
+    val idf = new IDF().setInputCol(columnName+"_ct_vectorized").setOutputCol(columnName+"_tfidf")
+    val idfModel = idf.fit(count_vectorizer_df)
+    val rescaledData = idfModel.transform(count_vectorizer_df)
+
+    rescaledData
+  }
+  def naiveBayes(dataFrame: DataFrame,indexer: StringIndexer,assembler: VectorAssembler): Unit ={
+    val Array(train_data, test_data) = dataFrame.randomSplit(Array(0.7,0.3))
+    val nb = new NaiveBayes()
+      .setLabelCol("target")
+    val pipeline = new Pipeline().setStages(Array(indexer,assembler,nb))
+    val model = pipeline.fit(train_data)
+
+    //model.write.overwrite().save("src/test/scala/resources/model/NaiveBayes")
+    val predictions = model.transform(test_data)
+    predictions.show()
+
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("target")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(predictions)
+
+    println(s"Naive Bayes Test set accuracy = $accuracy")
+  }
+  def randomForestClassifier(dataFrame: DataFrame,indexer: StringIndexer,assembler: VectorAssembler): Unit ={
+
+    // Splitting Data Frame
+    val Array(train_data, test_data) = dataFrame.randomSplit(Array(0.7,0.3))
+
+    ///// Random Forest Classifier
+    val rf = new RandomForestClassifier()
+      .setLabelCol("target")
+      .setFeaturesCol("features")
+      .setNumTrees(20)
+      .setPredictionCol("fake_predict")
+      .setMaxDepth(7)
+
+    val pipeline = new Pipeline().setStages(Array(indexer,assembler,rf))
+    val model = pipeline.fit(train_data)
+    model.write.overwrite().save("src/test/scala/resources/model/RandomForestClassifier")
+    val predictions = model.transform(test_data)
+    predictions.show()
+
+    // Select (prediction, true label) and compute test error.
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("target")
+      .setPredictionCol("fake_predict")
+      .setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(predictions)
+
+    println("Random Forest test set Accuracy = "+accuracy)
+
+  }
+  def pipeline_stages():(StringIndexer,VectorAssembler)={
+    // Converting labels to label indices
+    val indexer = new StringIndexer().setInputCol("subject").setOutputCol("subject_idx")
+
+    // Joining all the transformed columns
+    val assembler = new VectorAssembler().setInputCols(Array("title_tfidf","text_tfidf","subject_idx")).setOutputCol("features")
+    (indexer,assembler)
+  }
 }
